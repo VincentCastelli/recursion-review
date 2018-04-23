14ec32a91f3662f195ac906a59de6eed0b943d2e
VincentCastelli <vincentsalvatorecastelli@gmail.com>
2018-04-23 12:33:29 -0700

    Pushing stringifyJSON updates.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,36 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (typeof obj === 'boolean') {
+    if (obj === true) {
+      return 'true';
+    } else {
+      return 'false';
+    }
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+      var results = '';
+      _.each(obj, function(el) {
+        results += stringifyJSON(el);
+      });
+      return '[' + results + ']'; 
+    }
+  }
 };
